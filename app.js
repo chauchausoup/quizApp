9e6b71b1e81b5ce92ee36a53bb9253bd9f52f674
chauchausoup <7krishna7dhakal7@gmail.com>
2020-05-17 17:23:07 +0545

    after doing some hard coding we got a bug fixed

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ fetch("./data.json")
   .then((response) => response.json())
   .then((data) => (json = data));
 
-
 //lets select some queries
 
 var box = document.querySelectorAll(".cb");
@@ -24,62 +23,55 @@ var span = document.getElementsByTagName("span");
 
 var newBtn = document.createElement("BUTTON");
 var newResultBtn = document.createElement("BUTTON");
-var homeBtn = document.createElement("BUTTON");
-
-
-
-
-
-
-
+//var homeBtn = document.createElement("BUTTON");
 
 //some counters and flags
 
-var number= 1;
-var number2=1;
-
+var number = 2;
+var number2 = 1;
+var starter=1;
 var flag = 1;
 var res = 0;
-
-
+var resultArray=[];
 var optionSelected;
 
 //at very first of loading
-document.querySelector(".checkbox").style.display = "none";
-
+document.querySelector(".container").style.display = "none";
+document.querySelector(".starter").style.display = "block";
 
 //making changes to new buttons
 newBtn.innerHTML = "Go Home";
 newResultBtn.innerHTML = "Show Result";
-button.innerHTML = "";
-homeBtn.innerHTML = "Go";
-button.append(homeBtn);
-
+//button.innerHTML = "";
+//homeBtn.innerHTML = "Go";
+//button.append(homeBtn);
 
 box.forEach((b) => b.addEventListener("click", boxHighlighter));
-next.addEventListener("click", start);
-homeBtn.addEventListener("click",start);
+next.addEventListener("click", showNext);
+//homeBtn.addEventListener("click",showNext);
 
-
-
+//home page button function
+function homepage() {
+  document.querySelector(".container").style.display = "block";
+  showNext();
+}
 
 //goes home on showing result
 
-function go(){
+function go() {
   location.reload();
 }
 
 //highlights the single box when selected
 
 function boxHighlighter() {
-
-
-  window.optionSelected = Array.from(this.childNodes);
-
   if (!this.classList.contains("h") && flag === 1) {
     this.classList.add("h");
 
-  
+
+    window.optionSelected = Array.from(this.childNodes);
+
+    resultArray.push(window.optionSelected[3].textContent); 
 
     flag = 0;
     //num++;
@@ -87,23 +79,41 @@ function boxHighlighter() {
   }
   if (this.classList.contains("h") && flag === 0) {
     this.classList.remove("h");
-    
     flag = 1;
     return;
   }
 }
 
-
-
-
-
 // lets make a showNext app engine
 
 function showNext() {
+  
+if(starter===1){
+    var k = 0;
+    var l = 0;
+    document.querySelector(".checkbox").style.display = "block";
+    qOne.innerHTML = json[0].question;
+    pNo.forEach((p) => (p.innerHTML = 1));
+    sNo.forEach((p) => {
+      p.innerHTML = json[0].options[k].number;
+      k++;
+    });
+    optionValue.forEach((p) => {
+      p.innerHTML = json[0].options[l].option;
+      l++;
+    });
+  
+  qNumber.innerHTML = 1;
+  starter++;
+  number=2;
+return;
 
-  // if the length is 3+1 
 
-  if (number === json.length + 1) {
+}
+if (number ===5) {
+
+  checkResult();
+
     question.innerHTML = "Over!!!";
     button.innerHTML = "";
     document.querySelector(".checkbox").style.display = "none";
@@ -114,15 +124,12 @@ function showNext() {
     button.append(newResultBtn);
     newResultBtn.addEventListener("click", showResult);
 
-
-
     //after question completion go home
     function goHome() {
-      console.log("go home");
+      
 
       location.reload();
     }
-
 
     // what happens after i click show result button?
 
@@ -143,12 +150,12 @@ function showNext() {
   } else {
     var i = 0;
     var j = 0;
-    document.querySelector(".checkbox").style.display = "none";
+    // document.querySelector(".checkbox").style.display = "none";
 
     document.querySelector(".checkbox").style.display = "block";
 
-    //qNumber.innerHTML = number;
-   
+    qNumber.innerHTML = number;
+
     qOne.innerHTML = json[number - 1].question;
     pNo.forEach((p) => (p.innerHTML = number));
     sNo.forEach((p) => {
@@ -158,50 +165,31 @@ function showNext() {
     optionValue.forEach((p) => {
       p.innerHTML = json[number - 1].options[i].option;
       i++;
-      
     });
-  }
-  qNumber.innerHTML = number;
   
-  checkResult();
-console.log(number)
+
   number++;
-console.log(number)
-  
+  checkResult();
+
 }
-
-
+}
 //check result()
 
-function checkResult(){
+function checkResult() {
+  var q=resultArray.pop();
+  var r=json[number2 - 1].correctOption;
 
 
-  console.log("hello");
-
-  
-    if (json[number2 - 1].correctOption === (window.optionSelected[3]).textContent) {
-      res++;
-      
-    
-    }
-    console.log(res)
-      
-    console.log(number2)
-  
-number2++;
-  return; 
+  if (  q===r){
+    res++;
+  console.log("INNER result:" ,res);
+  }
 
 
+  console.log("OUTER result:" ,res);
+
+  number2++;
+  return;
 }
-
 
 //start the show
-
-function start(){
-
-
-
-  showNext();
-
-  
-}
